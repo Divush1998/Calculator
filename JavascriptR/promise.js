0cb39f692d35323427d702c582bcee30dbc0cc1e
Vishal Kumar <vishal.kumar@codestoresolutions.com>
2025-05-15 13:02:47 +0530

    Initial commit

--- JavascriptR/promise.js
+++
@@ -1,8 +0,0 @@
-let checkEven = new Promise((success, unsuccess) => {
-  let num = 10;
-  if (num % 2 === 0) success("Number is even");
-  else unsuccess("Number is not even");
-});
-checkEven
-  .then((message) => console.log(message))
-  .catch((error) => console.error(error));
